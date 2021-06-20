37fadf207cc6a7daf52ac7f4d8cfa25533c03dfa
loleksatyr <czarnotabartek@gmail.com>
2021-06-20 21:12:42 +0200

    Goblinadd

---
+++ Goblinz/Assets/Scripts/GoblinController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour
+{
+    [SerializeField] GameObject[] points;
+    float moveSpeed = 3f;
+    bool edge = false;
+    [SerializeField] int health = 2;
+    Animator animator;
+    bool isdead = false;
+    [SerializeField] GameObject _coinprefab;
+
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points.Length == 0 || points.Length == 1)
+        {
+            animator.SetBool("Idle", true);
+        }
+        else
+        {
+            animator.SetBool("Run", true);
+            animator.SetBool("Idle", false);
+            if (isdead == false)
+            {
+                if (edge)
+                {
+                    transform.position -= Vector3.right * moveSpeed * Time.deltaTime;
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+                    spriteRenderer.flipX = false;
+                }
+            }
+        }
+        if (health == 0)
+        {
+            animator.SetBool("Die", true);
+
+            isdead = true;
+            Invoke("Disappear", 1f);
+            health--;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "point")
+        {
+            edge = !edge;
+            Debug.Log(edge);
+        }
+        Debug.Log("trigger");
+
+        if (collision.gameObject.tag == "Damage")
+        {
+            health--;
+
+        }
+    }
+    private void Disappear()
+    {
+        gameObject.SetActive(false);
+        Instantiate(_coinprefab, transform.position, Quaternion.identity);
+
+
+    }
+}
+
